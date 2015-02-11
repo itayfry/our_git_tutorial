dcf4eca8f5ffbcdf1d380b404fc4cc5c639ff15e
itayfry <itayfry@gmail.com>
2015-02-11 23:08:00 +0200

    another commit which will cause a conflict

--- first.php
+++ first.php
@@ -6,7 +6,7 @@
  * Time: 10:07 PM
  */
 
-// dev 2 feature branch
+// dev 2 feature branch second commit
 function doSomething(){
     echo "I didn't have a lot of time to think of a better function";
 }
