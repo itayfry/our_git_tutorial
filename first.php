c47f7beb00f7b23dd0e27597223a59b1dbacd500
itayfry <itayfry@gmail.com>
2015-02-11 22:12:47 +0200

    adding first.php and .gitignore

---
+++ first.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: itay.friedman
+ * Date: 2/11/15
+ * Time: 10:07 PM
+ */
+
+function doSomething(){
+    echo "I didn't have time to think of a better function";
+}
